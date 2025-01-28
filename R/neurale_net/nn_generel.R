e8494369007b657345dc9fb95c9e3d162c164845
rubak <rubak@math.aau.dk>
2025-01-28 09:18:40 +0100

    Kode til neurale net med flere klasser (softmax).

--- R/neurale_net/nn_generel.R
+++ R/neurale_net/nn_generel.R
@@ -3,12 +3,16 @@ sigmoid <- function(x) {
   1 / (1 + exp(-x))
 }
 
+softmax <- function(x) {
+  exp(x) / rowSums(exp(x))
+}
+
 # Initialize weights and biases
-initialize_parameters <- function(n, n1, n2, const = NA) {
+initialize_parameters <- function(n, n1, n2, num_classes = 1, const = NA) {
   two_hidden <- TRUE
   if(n2==0){
     two_hidden <- FALSE
-    n2 <- 1
+    n2 <- num_classes
   }
   if(is.na(const)){
     weight_fun <- runif
@@ -22,8 +26,8 @@ initialize_parameters <- function(n, n1, n2, const = NA) {
     b2 = matrix(weight_fun(n2), nrow = n2, ncol = 1)
   )
   if(two_hidden){
-    out$W3 <- matrix(weight_fun(n2), nrow = 1, ncol = n2)
-    out$b3 <- weight_fun(1)
+    out$W3 <- matrix(weight_fun(n2*num_classes), nrow = num_classes, ncol = n2)
+    out$b3 <- matrix(weight_fun(num_classes), nrow = num_classes, ncol = 1)
   }
   return(out)
 }
@@ -33,21 +37,40 @@ forward_propagation <- function(X, params) {
   Z1 <- params$W1 %*% X + matrix(params$b1, nrow = length(params$b1), ncol = ncol(X))
   A1 <- sigmoid(Z1)
   Z2 <- params$W2 %*% A1 + matrix(params$b2, nrow = length(params$b2), ncol = ncol(A1))
-  A2 <- sigmoid(Z2)
-  out <- list(A2 = A2, A1 = A1, Z2 = Z2, Z1 = Z1)
+  out <- list(A1 = A1, Z2 = Z2, Z1 = Z1)
   if(!is.null(params$W3)){
-    out$Z3 <- params$W3 %*% A2 + matrix(params$b3, nrow = length(params$b3), ncol = ncol(A2))
-    out$A3 <- sigmoid(out$Z3)
+    out$A2 <- sigmoid(Z2)
+    out$Z3 <- params$W3 %*% out$A2 + matrix(params$b3, nrow = length(params$b3), ncol = ncol(out$A2))
+    if(length(params$b3)>1){
+      out$A3 <- softmax(t(out$Z3))
+    } else{
+      out$A3 <- sigmoid(out$Z3)
+    }
+  } else{
+    if(length(params$b2)>1){
+      out$A2 <- softmax(t(out$Z2))
+    } else{
+      out$A2 <- sigmoid(out$Z2)
+    }
   }
   return(out)
 }
 
 # Compute the loss (binary cross-entropy)
 compute_loss <- function(Y, output, loss_function = c("cross-entropy", "squared")) {
+  num_classes <- nrow(Y)
   if(loss_function == "squared"){
-    return(1/2*sum((Y - output)^2))
+    if(is.null(num_classes)){
+      return(1/2*((Y - output)^2))
+    } else{
+      return(1/2*rowSums((Y - output)^2))
+    }
   } else if(loss_function == "cross-entropy"){
-    return(mean(-Y * log(output) - (1 - Y) * log(1 - output)))
+    if(is.null(num_classes)){
+      return(mean(-Y * log(output) - (1 - Y) * log(1 - output)))
+    } else{
+      return(-mean(rowSums(Y * log(output))))
+    }
   } else{
     stop("Unknown loss function")
   }
@@ -57,10 +80,19 @@ compute_loss <- function(Y, output, loss_function = c("cross-entropy", "squared"
 backward_propagation <- function(X, Y, params, cache, loss_function = "cross-entropy") {
   m <- ncol(X)
   loss_grad <- function(Y, output, loss_function){
+    num_classes <- nrow(Y)
     if(loss_function == "cross-entropy"){
-      return(output - Y)
+      if(is.null(num_classes)){
+        return(output - Y)
+      } else{
+        return(t(output) - Y)
+      }
     } else if(loss_function == "squared"){
-      return((output - Y) * output * (1 - output))
+      if(is.null(num_classes)){
+        return((output - Y) * output * (1 - output))
+      } else{
+        return((t(output) - Y) * t(output) * (1 - t(output)))
+      }
     } else{
       stop("Unknown loss function")
     }
@@ -111,7 +143,7 @@ train_neural_network <- function(X, Y, n1, n2, iterations, learning_rate, params
   }
   n <- nrow(X)
   if(is.null(params)){
-    params <- initialize_parameters(n, n1, n2)
+    params <- initialize_parameters(n, n1, n2, num_classes = nrow(Y))
   }
   cache_list <- loss_list <- grads_list <- params_list <- list()
   for (i in 1:iterations) {
@@ -134,28 +166,28 @@ nn_fun <- function(formula, data, weights = NA, n_hidden = c(1,1), activation = 
   colnames(x)[colnames(x)=="(Intercept)"] <- "bias"
   y_name <- as.character(formula)[2]
   y <- data[[y_name]]
-  # Ensure y is a factor
-  if(!is.factor(y)){
-    y <- factor(y)
+  
+  if(is.character(y) | is.factor(y)){
+    # Ensure y is a factor
+    if(!is.factor(y)){
+      y <- factor(y)
+    }
+    if(length(levels(y))==1){stop("Response variable must have at least two levels")}
+    if(length(levels(y))==2){
+      y <- ifelse(y==levels(y)[1], 1, 0)
+    } else{
+      y <- model.matrix(~y-1, data = data)
+    }
   }
-  # If it is a -1/+1 variable we make +1 the first level for backwards compatibility
-  if(identical(levels(y), c("-1","1"))){
-    y <- relevel(y, "1")
-  }
-  # Code first factor level as 1 and remaining as -1
-  y <- ifelse(y==levels(y)[1], 1, -1)
-  # Change from -1 to 0 for sigmoid-case.
-  if(activation=="sigmoid"){
-    y[y==-1] <- 0
-  }
+  y <- t(y)
   X <- t(x[,colnames(x)!="bias"])
   if(length(weights)==1){
-    params <- initialize_parameters(nrow(X), n_hidden[1], n_hidden[2], const = weights)
+    params <- initialize_parameters(nrow(X), n_hidden[1], n_hidden[2], const = weights, num_classes = nrow(y))
   } else{
-    params <- initialize_parameters(nrow(X), n_hidden[1], n_hidden[2])
+    params <- initialize_parameters(nrow(X), n_hidden[1], n_hidden[2], num_classes = nrow(y))
   }
   # browser()
-  train_neural_network(X, as.numeric(t(y)), n1 = n_hidden[1], n2 = n_hidden[2], iterations = max_it, learning_rate = eta, params = params)
+  train_neural_network(X, y, n1 = n_hidden[1], n2 = n_hidden[2], iterations = max_it, learning_rate = eta, params = params)
 }
 # 
 # # Example usage
