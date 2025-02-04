72d298a538fe4b82f34cf97e21ae2c584b69a93f
rubak <rubak@math.aau.dk>
2025-02-04 17:01:48 +0100

    Mere kode til neurale net.

--- R/neurale_net/nn_generel.R
+++ R/neurale_net/nn_generel.R
@@ -60,14 +60,14 @@ forward_propagation <- function(X, params) {
 compute_loss <- function(Y, output, loss_function = c("cross-entropy", "squared")) {
   num_classes <- nrow(Y)
   if(loss_function == "squared"){
-    if(is.null(num_classes)){
-      return(1/2*((Y - output)^2))
+    if(is.null(num_classes) || num_classes==1){
+      return(1/2*((Y - t(output))^2))
     } else{
       return(1/2*rowSums((Y - output)^2))
     }
   } else if(loss_function == "cross-entropy"){
-    if(is.null(num_classes)){
-      return(mean(-Y * log(output) - (1 - Y) * log(1 - output)))
+    if(is.null(num_classes) || num_classes==1){
+      return(mean(-Y * log(t(output)) - (1 - Y) * log(1 - t(output))))
     } else{
       return(-mean(rowSums(Y * log(output))))
     }
@@ -82,13 +82,13 @@ backward_propagation <- function(X, Y, params, cache, loss_function = "cross-ent
   loss_grad <- function(Y, output, loss_function){
     num_classes <- nrow(Y)
     if(loss_function == "cross-entropy"){
-      if(is.null(num_classes)){
+      if(is.null(num_classes) || num_classes==1){
         return(output - Y)
       } else{
         return(t(output) - Y)
       }
     } else if(loss_function == "squared"){
-      if(is.null(num_classes)){
+      if(is.null(num_classes) || num_classes==1){
         return((output - Y) * output * (1 - output))
       } else{
         return((t(output) - Y) * t(output) * (1 - t(output)))
@@ -99,19 +99,19 @@ backward_propagation <- function(X, Y, params, cache, loss_function = "cross-ent
   }
   if(!is.null(params$W3)){
     dZ3 <- loss_grad(Y, cache$A3, loss_function)
-    dW3 <- (1 / m) * dZ3 %*% t(cache$A2)
-    db3 <- (1 / m) * sum(dZ3)
+    dW3 <- dZ3 %*% t(cache$A2)
+    db3 <- rowSums(dZ3)
     dA2 <- t(params$W3) %*% dZ3
   } else{
     dA2 <- loss_grad(Y, cache$A2, loss_function)
   }
   dZ2 <- dA2 * cache$A2 * (1 - cache$A2)
-  dW2 <- (1 / m) * dZ2 %*% t(cache$A1)
-  db2 <- (1 / m) * sum(dZ2)
+  dW2 <- dZ2 %*% t(cache$A1)
+  db2 <- rowSums(dZ2)
   dA1 <- t(params$W2) %*% dZ2
   dZ1 <- dA1 * cache$A1 * (1 - cache$A1)
-  dW1 <- (1 / m) * dZ1 %*% t(X)
-  db1 <- (1 / m) * sum(dZ1)
+  dW1 <- dZ1 %*% t(X)
+  db1 <- rowSums(dZ1)
   out <- list(dW1 = dW1, db1 = db1, dW2 = dW2, db2 = db2)
   if(!is.null(params$W3)){
     out$dW3 <- dW3
@@ -148,8 +148,8 @@ train_neural_network <- function(X, Y, n1, n2, iterations, learning_rate, params
   cache_list <- loss_list <- grads_list <- params_list <- list()
   for (i in 1:iterations) {
     cache_list[[i]] <- cache <- forward_propagation(X, params)
-    output <- ifelse(!is.null(params$W3), cache$A3, cache$A2)
-    loss_list[[i]] <- loss <- compute_loss(Y, output, loss_function)
+    output <- if(!is.null(params$W3)){cache$A3} else{cache$A2}
+    loss_list[[i]] <- loss <- compute_loss(Y, t(output), loss_function)
     grads_list[[i]] <- grads <- backward_propagation(X, Y, params, cache, loss_function = loss_function)
     params_list[[i]] <- params <- update_parameters(params, grads, learning_rate)
     if (i %% 100 == 0) {
@@ -190,14 +190,18 @@ nn_fun <- function(formula, data, weights = NA, n_hidden = c(1,1), activation = 
   train_neural_network(X, y, n1 = n_hidden[1], n2 = n_hidden[2], iterations = max_it, learning_rate = eta, params = params)
 }
 # 
-# # Example usage
+# Example usage
 # set.seed(123)
+# n_sample <- 5
 # n <- 3  # Number of input neurons
-# n1 <- 5  # Number of neurons in the first hidden layer
+# n1 <- 2  # Number of neurons in the first hidden layer
 # n2 <- 4  # Number of neurons in the second hidden layer
-# X <- matrix(runif(n * 10), nrow = n)  # Example input data
-# Y <- matrix(sample(0:1, 10, replace = TRUE), nrow = 1)  # Example output data
+# X <- matrix(runif(n * n_sample), nrow = n)  # Example input data
+# # Y <- matrix(sample(0:1, n_sample, replace = TRUE), nrow = 1)  # Example output data
+# Y <- replicate(n_sample, sample(c(1,0,0)))
 # iterations <- 1000
 # learning_rate <- 0.01
 # 
-# trained_params <- train_neural_network(X, Y, n, n1, n2, iterations, learning_rate)
+# trained_params <- train_neural_network(X, Y, n1, n2, iterations, learning_rate)
+
+# nn_fun(Species ~ ., iris, n_hidden = c(2,2), eta = 0.01, max_it = 1000, lossfun = "cross-entropy")
--- R/neurale_net/nn_viz.R
+++ R/neurale_net/nn_viz.R
@@ -1,13 +1,17 @@
 library(visNetwork)
 visualize_neural_network_interactive <- function(params, cache) {
-  n <- params$W1 |> ncol()
+  n <- ncol(params$W1)
   n1 <- params$W1 |> nrow()
   n2 <- params$W2 |> nrow()
+  num_classes <- params$W3 |> nrow()
   nodes <- data.frame(
-    id = c(1:n, (n+1):(n+n1), (n+n1+1):(n+n1+n2), n+n1+n2+1),
-    label = c(paste0("X_", 1:n), paste0("Y_", 1:n1), paste0("Z_", 1:n2), "O"),
-    group = c(rep("Input", n), rep("Hidden1", n1), rep("Hidden2", n2), "Output"),
-    level = c(rep(1, n), rep(2, n1), rep(3, n2), 4)
+    # id = c(1:n, (n+1):(n+n1), (n+n1+1):(n+n1+n2), (n+n1+n2+1):(n+n1+n2+num_classes)),
+    id = 1:(n+1+n1+1+n2+1+num_classes),
+    label = c("X_bias", paste0("X_", 1:n), "Y_bias", paste0("Y_", 1:n1), "Z_bias", paste0("Z_", 1:n2), paste0("O_", 1:num_classes)),
+    group = c(rep("Input", n+1), rep("Hidden1", n1+1), rep("Hidden2", n2+1), rep("Output", num_classes)),
+    level = c(rep(1, n+1), rep(2, n1+1), rep(3, n2+1), rep(4, num_classes)),
+    shape = c("box", rep("circle", n), "box", rep("circle", n1), "box", rep("circle", n2), rep("ellipse", num_classes)),
+    title = c("Bias", paste0("Input ", 1:n), "Bias", paste0("Hidden1 ", 1:n1), "Bias", paste0("Hidden2 ", 1:n2), paste0("Output ", 1:num_classes))
   )
   # nodes$title <- nodes$label
   # nodes$title[(n+1):(n+n1)] <- paste0(nodes$title[(n+1):(n+n1)], "=", round(cache$A1, 2))
@@ -20,23 +24,46 @@ visualize_neural_network_interactive <- function(params, cache) {
     title = character()
   )
   
+  # Bias 1
+    for (j in 1:n1) {
+      weight <- round(params$b1[j, 1], 3)
+      edges <- rbind(edges, data.frame(from = 1, to = n+2 + j, value = abs(weight), label = weight, title = paste("Weight:", weight)))
+    }
+  
+  # Layer 1
   for (i in 1:n) {
     for (j in 1:n1) {
-      weight <- round(params$W1[j, i], 2)
-      edges <- rbind(edges, data.frame(from = i, to = n + j, value = weight, label = weight, title = paste("Weight:", weight)))
+      weight <- round(params$W1[j, i], 3)
+      edges <- rbind(edges, data.frame(from = i+1, to = n+2 + j, value = abs(weight), label = weight, title = paste("Weight:", weight)))
     }
   }
   
+  # Bias 2
+  for (j in 1:n2) {
+    weight <- round(params$b2[j, 1], 3)
+    edges <- rbind(edges, data.frame(from = n+2, to = n+3+n1 + j, value = abs(weight), label = weight, title = paste("Weight:", weight)))
+  }
+
+  # Layer 2
   for (i in 1:n1) {
     for (j in 1:n2) {
-      weight <- round(params$W2[j, i], 2)
-      edges <- rbind(edges, data.frame(from = n + i, to = n + n1 + j, value = weight, label = weight, title = paste("Weight:", weight)))
+      weight <- round(params$W2[j, i], 3)
+      edges <- rbind(edges, data.frame(from = n+2 + i, to = n+3 + n1 + j, value = abs(weight), label = weight, title = paste("Weight:", weight)))
     }
   }
   
+  # Bias 3
+  for (j in 1:num_classes) {
+    weight <- round(params$b3[j, 1], 3)
+    edges <- rbind(edges, data.frame(from = n+3+n1, to = n+3+n1+n2 + j, value = abs(weight), label = weight, title = paste("Weight:", weight)))
+  }
+  
+  # Layer 3
   for (i in 1:n2) {
-    weight <- round(params$W3[1, i], 2)
-    edges <- rbind(edges, data.frame(from = n + n1 + i, to = n + n1 + n2 + 1, value = weight, label = weight, title = paste("Weight:", weight)))
+    for (j in 1:num_classes) {
+      weight <- round(params$W3[j, i], 3)
+      edges <- rbind(edges, data.frame(from = n+3+n1 + i, to = n+3 + n1 + n2 + j, value = abs(weight), label = weight, title = paste("Weight:", weight)))
+    }
   }
   
   visNetwork(nodes, edges) %>%
@@ -49,10 +76,10 @@ visualize_neural_network_interactive <- function(params, cache) {
 # Example usage
 set.seed(123)
 n <- 3  # Number of input neurons
-n1 <- 1  # Number of neurons in the first hidden layer
-n2 <- 1  # Number of neurons in the second hidden layer
-X <- matrix(runif(n * 10), nrow = n)  # Example input data
-params <- initialize_parameters(n, n1, n2)
+n1 <- 2  # Number of neurons in the first hidden layer
+n2 <- 4  # Number of neurons in the second hidden layer
+X <- matrix(runif(n * 5), nrow = n)  # Example input data
+params <- initialize_parameters(n, n1, n2, num_classes = 2)
 cache <- forward_propagation(X, params)
 
 visualize_neural_network_interactive(params, cache)
