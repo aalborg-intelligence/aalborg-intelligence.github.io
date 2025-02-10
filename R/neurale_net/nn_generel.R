a22c64e89df6b2e6cef533c42c92f9a6aba76be9
rubak <rubak@math.aau.dk>
2025-02-10 16:56:56 +0100

    Tillad 0 skjulte lag.

--- R/neurale_net/nn_generel.R
+++ R/neurale_net/nn_generel.R
@@ -9,10 +9,14 @@ softmax <- function(x) {
 
 # Initialize weights and biases
 initialize_parameters <- function(n, n1, n2, num_classes = 1, const = NA) {
-  two_hidden <- TRUE
+  n_hidden_layers <- 2
   if(n2==0){
-    two_hidden <- FALSE
+    n_hidden_layers <- 1
     n2 <- num_classes
+  }
+  if(n1==0){
+    n_hidden_layers <- 0
+    n1 <- num_classes
   }
   if(is.na(const)){
     weight_fun <- runif
@@ -21,11 +25,13 @@ initialize_parameters <- function(n, n1, n2, num_classes = 1, const = NA) {
   }
   out <- list(
     W1 = matrix(weight_fun(n * n1), nrow = n1, ncol = n),
-    b1 = matrix(weight_fun(n1), nrow = n1, ncol = 1),
-    W2 = matrix(weight_fun(n1 * n2), nrow = n2, ncol = n1),
-    b2 = matrix(weight_fun(n2), nrow = n2, ncol = 1)
+    b1 = matrix(weight_fun(n1), nrow = n1, ncol = 1)
   )
-  if(two_hidden){
+  if(n_hidden_layers>0){
+    out$W2 <- matrix(weight_fun(n1 * n2), nrow = n2, ncol = n1)
+    out$b2 <- matrix(weight_fun(n2), nrow = n2, ncol = 1)
+  }
+  if(n_hidden_layers>1){
     out$W3 <- matrix(weight_fun(n2*num_classes), nrow = num_classes, ncol = n2)
     out$b3 <- matrix(weight_fun(num_classes), nrow = num_classes, ncol = 1)
   }
@@ -36,10 +42,20 @@ initialize_parameters <- function(n, n1, n2, num_classes = 1, const = NA) {
 forward_propagation <- function(X, params) {
   Z1 <- params$W1 %*% X + matrix(params$b1, nrow = length(params$b1), ncol = ncol(X))
   A1 <- sigmoid(Z1)
-  Z2 <- params$W2 %*% A1 + matrix(params$b2, nrow = length(params$b2), ncol = ncol(A1))
-  out <- list(A1 = A1, Z2 = Z2, Z1 = Z1)
+  out <- list(Z1 = Z1, A1 = A1)
+  if(is.null(params$W2)){ # No hidden layers
+    if(length(params$b1)>1){ # Change to softmax if num_classes > 1
+      out$A1 <- softmax(t(Z1))
+    }
+    return(out)
+  }
+  # Now there is at least one hidden layer
+  out$Z2 <- try(params$W2 %*% A1 + matrix(params$b2, nrow = length(params$b2), ncol = ncol(A1)))
+  # if(inherits(out$Z2, "try-error")){
+  #   browser()
+  # }
   if(!is.null(params$W3)){
-    out$A2 <- sigmoid(Z2)
+    out$A2 <- sigmoid(out$Z2)
     out$Z3 <- params$W3 %*% out$A2 + matrix(params$b3, nrow = length(params$b3), ncol = ncol(out$A2))
     if(length(params$b3)>1){
       out$A3 <- softmax(t(out$Z3))
@@ -97,22 +113,30 @@ backward_propagation <- function(X, Y, params, cache, loss_function = "cross-ent
       stop("Unknown loss function")
     }
   }
-  if(!is.null(params$W3)){
+  if(!is.null(params$W3)){ # Two hidden layers
     dZ3 <- loss_grad(Y, cache$A3, loss_function)
     dW3 <- dZ3 %*% t(cache$A2)
     db3 <- rowSums(dZ3)
     dA2 <- t(params$W3) %*% dZ3
-  } else{
-    dA2 <- loss_grad(Y, cache$A2, loss_function)
+    dZ2 <- dA2 * cache$A2 * (1 - cache$A2)
+  } else if(!is.null(params$W2)){ # One hidden layers
+    dZ2 <- loss_grad(Y, cache$A2, loss_function)
   }
-  dZ2 <- dA2 * cache$A2 * (1 - cache$A2)
-  dW2 <- dZ2 %*% t(cache$A1)
-  db2 <- rowSums(dZ2)
-  dA1 <- t(params$W2) %*% dZ2
-  dZ1 <- dA1 * cache$A1 * (1 - cache$A1)
+  if(!is.null(params$W2)){ # One hidden layer
+    dW2 <- dZ2 %*% t(cache$A1)
+    db2 <- rowSums(dZ2)
+    dA1 <- t(params$W2) %*% dZ2
+    dZ1 <- dA1 * cache$A1 * (1 - cache$A1)
+  } else{ # No hidden layers
+    dZ1 <- loss_grad(Y, cache$A1, loss_function)
+  }
   dW1 <- dZ1 %*% t(X)
   db1 <- rowSums(dZ1)
-  out <- list(dW1 = dW1, db1 = db1, dW2 = dW2, db2 = db2)
+  out <- list(dW1 = dW1, db1 = db1)
+  if(!is.null(params$W2)){
+    out$dW2 <- dW2
+    out$db2 <- db2
+  }
   if(!is.null(params$W3)){
     out$dW3 <- dW3
     out$db3 <- db3
@@ -124,8 +148,10 @@ backward_propagation <- function(X, Y, params, cache, loss_function = "cross-ent
 update_parameters <- function(params, grads, learning_rate) {
   params$W1 <- params$W1 - learning_rate * grads$dW1
   params$b1 <- params$b1 - learning_rate * grads$db1
-  params$W2 <- params$W2 - learning_rate * grads$dW2
-  params$b2 <- params$b2 - learning_rate * grads$db2
+  if(!is.null(params$W2)){
+    params$W2 <- params$W2 - learning_rate * grads$dW2
+    params$b2 <- params$b2 - learning_rate * grads$db2
+  }
   if(!is.null(params$W3)){
     params$W3 <- params$W3 - learning_rate * grads$dW3
     params$b3 <- params$b3 - learning_rate * grads$db3
@@ -135,10 +161,7 @@ update_parameters <- function(params, grads, learning_rate) {
 
 # Train the neural network
 train_neural_network <- function(X, Y, n1, n2, iterations, learning_rate, params = NULL, loss_function = "cross-entropy") {
-  if(n1==0 & n2==0){
-    stop("At least one hidden layer is required")
-  }
-  if(n1==0){
+  if(n1==0 & n2>0){
     n1 <- n2
   }
   n <- nrow(X)
@@ -148,11 +171,11 @@ train_neural_network <- function(X, Y, n1, n2, iterations, learning_rate, params
   cache_list <- loss_list <- grads_list <- params_list <- list()
   for (i in 1:iterations) {
     cache_list[[i]] <- cache <- forward_propagation(X, params)
-    output <- if(!is.null(params$W3)){cache$A3} else{cache$A2}
+    output <- if(!is.null(params$W3)){cache$A3} else{ if(!is.null(params$W2)){cache$A2} else{cache$A1} }
     loss_list[[i]] <- loss <- compute_loss(Y, t(output), loss_function)
     grads_list[[i]] <- grads <- backward_propagation(X, Y, params, cache, loss_function = loss_function)
     params_list[[i]] <- params <- update_parameters(params, grads, learning_rate)
-    if (i %% 100 == 0) {
+    if (i %% 1000 == 0) {
       cat("Iteration", i, "loss:", loss, "\n")
     }
   }
@@ -206,4 +229,4 @@ nn_fun <- function(formula, data, weights = NA, n_hidden = c(1,1), activation = 
 ir <- iris
 ir[,1:4] <- scale(ir[,1:4])
 # ir <- ir[c(1:3, 51:53, 101:103),]
-fit_ir <- nn_fun(Species ~ ., ir, n_hidden = c(9,5), eta = 0.1, max_it = 10000, lossfun = "cross-entropy")
+fit_ir <- nn_fun(Species ~ ., ir, n_hidden = c(0,0), eta = 0.05, max_it = 20000, lossfun = "cross-entropy")
