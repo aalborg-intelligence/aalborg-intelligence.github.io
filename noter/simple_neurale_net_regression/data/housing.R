ed007533774c9cc3433b58af863b12e7a589ed63
MaleneCramerEngebjerg <cr@aalborghus.dk>
2026-02-24 16:05:56 +0100

    Videre med regressionsnote - ikke færdig.

--- noter/simple_neurale_net_regression/data/housing.R
+++ noter/simple_neurale_net_regression/data/housing.R
@@ -11,9 +11,26 @@ print(c(bias = b, w))
 
 pred <- predict(fit, dat, type = "response")
 
+sqrt(mean((pred-dat$price)^2))
+
+plot(dat$price,pred)
+abline(0,1)
+
 fit_lm <- lm(price ~ area + bedrooms + bathrooms, data = dat)
 summary(fit_lm)
 
 ## Malenes
 summary(dat$bathrooms)
 pairs(dat[,c("price", "bedrooms", "area", "bathrooms","basement")])
+
+fitNN <- nn_fun(price ~ area + bedrooms + bathrooms + basement, data = dat, 
+                n_hidden = c(40, 0), iter = 10000, type = "regression", 
+                lossfun = "squared", eta = .00001, activation = "Sigmoid")
+
+predNN <- predict(fitNN, dat, type = "response")
+
+sum((dat$price-mean(dat$price))^2)/2
+sqrt(mean((predNN-dat$price)^2))
+
+plot(dat$price,predNN)
+abline(0,1)
