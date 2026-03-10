c3fbab346f545eff267287fafc602885f7e376fa
MaleneCramerEngebjerg <cr@aalborghus.dk>
2026-03-10 16:17:18 +0100

    Videre med regressionsnote.

--- noter/simple_neurale_net_regression/data/home.R
+++ noter/simple_neurale_net_regression/data/home.R
@@ -25,13 +25,10 @@ path = here::here("noter", "simple_neurale_net_regression", "data", "home_Aalbor
 )
 
 home_small_lejlighed <- homedata |>
-  filter(Postnr %in% c("9000"), EjdType == "Ejerlejlighed") |> 
-  select(pris = "Pris_Salg", areal = "Areal_Bolig", alder = "Alder", 
-         altan = "Ejd_Altan", ejerudgift = "Beloeb_EjerUdgift", 
-         antalrum = "Ejd_AntalRum", toiletter = "Ejd_AntalToiletter",
-         dist_raadhus = "Dist_raadhus", add = "Adresse_Fuld", salgsaar = "Salgsaar") |> 
-  mutate(pris_mio=pris/1000000, aar = salgsaar-2010)
-
+  filter(Postnr %in% c("9000"), EjdType == "Ejerlejlighed", !is.na(Alder)) |> 
+  mutate(pris_mio=Pris_Salg/1000000, aar = Salgsaar-2010) |>
+  select(pris_mio, areal = "Areal_Bolig", alder = "Alder")  
+  
 summary(home_small_lejlighed)
 
 
@@ -40,6 +37,7 @@ writexl::write_xlsx(
   path = here::here("noter", "simple_neurale_net_regression", "data", "home_Aalborg9000_lejlighed.xlsx")
 )
 
+## Forskellige plots
 
 home_small_lejlighed |> 
   filter(pris_mio>5) |> 
@@ -63,11 +61,43 @@ home_small |>
 home_small |> 
   plot_ly(x = ~alder, y = ~areal, z = ~pris, type = "scatter3d", mode = "markers")
 
-fit_lm <- lm(pris ~ areal + alder + grund + toiletter, data = home_small)
+##
+
+## Modeller
+
+fit_lm <- lm(pris_mio ~ areal + alder, data = home_small_lejlighed)
 summary(fit_lm)
+
+pred <- predict(fit_lm, home_small_lejlighed, type = "response")
+plot(home_small_lejlighed$pris_mio,pred)
+abline(0,1)
+#fit_lm2 <- lm(pris ~ areal + alder, data = home_small_lejlighed)
+#summary(fit_lm2)
+
 
 fit <- nn_fun(pris_mio ~ areal + alder, data = na.omit(home_small_lejlighed), 
               n_hidden = c(0, 5), iter = 500, type = "regression", 
               lossfun = "squared", eta = .0001, weights=0, activation = "ReLu")
 pred <- predict(fit, newdata = na.omit(home_small_lejlighed), type = "response")
 plot(home_small_lejlighed$pris_mio, pred)
+
+
+
+## Bruges i noten
+
+dat <- readxl::read_excel(here::here("noter/simple_neurale_net_regression/data/home_Aalborg9000_lejlighed.xlsx"))
+fit_lm <- lm(pris_mio ~ areal + alder, data = dat)
+#fit <- nn_fun(price ~ area + bedrooms + bathrooms + basement, data = dat, n_hidden = c(0, 0), iter = #500, type = "regression", lossfun = "squared", eta = .0001, weights=0)
+#w <- as.vector(fit$params$W1)
+#b <- as.vector(fit$params$b1)
+#if(!is.null(fit$scale_val)){
+#  w <- w / fit$scale_val
+#  b <- b - sum(w * fit$center_val)
+#}
+pred <- predict(fit_lm, dat, type = "response")
+length(pred)
+summary(pred)
+plot(dat$pris_mio,pred, xlab="Lejlighedspris i millioner", ylab="Prædikteret pris", col="#020873")
+abline(0,1)
+summary(fit_lm)
+
